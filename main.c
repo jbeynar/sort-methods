62ede5d0ac2e6224faf83a94195ecd7645cc6b1b
jbeynar <jakub.bejnarowicz@gmail.com>
2018-06-01 11:56:16 +0200

    insertion sort

--- main.c
+++ main.c
@@ -14,11 +14,24 @@ void bubbleSort(int *array, int n) {
     }
 }
 
+void insertionSort(int *array, int n) {
+    int X;
+    for (int i = 1; i < n; i++) {
+        int j = i - 1;
+        X = array[i];
+        while (j >= 0 && X < array[j]) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = X;
+    }
+}
+
 int main() {
     int data[] = {4, 1, 89, 45, 2, 8, 10, 41, 4, 9, 10};
     int n = sizeof(data) / sizeof(data[0]);
-    bubbleSort(data, n);
-
+//    bubbleSort(data, n);
+    insertionSort(data, n);
 
     for (int i = 0; i < n; i++) {
         printf("%d, ", data[i]);
