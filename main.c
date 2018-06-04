56a90e38a3508262880f080663622a80d8e7cb68
jbeynar <jakub.bejnarowicz@gmail.com>
2018-06-04 13:09:05 +0200

    quicksort

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
 void bubbleSort(int *array, int n) {
-    int tmp;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n - 1 - i; j++) {
             if (array[j] > array[j + 1]) {
-                tmp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = tmp;
+                swap(&array[j], &array[j + 1]);
             }
         }
     }
@@ -28,7 +30,7 @@ void insertionSort(int *array, int n) {
 }
 
 void selectionSort(int *array, int n) {
-    int tmp, minIndex;
+    int minIndex;
     for (int i = 0; i < n; i++) {
         minIndex = i;
         for (int j = i + 1; j < n; j++) {
@@ -37,10 +39,31 @@ void selectionSort(int *array, int n) {
             }
         }
         if (i != minIndex) {
-            tmp = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = tmp;
+            swap(&array[minIndex], &array[i]);
         }
+    }
+}
+
+int partition(int *array, int left, int right) {
+    int pivot = array[right];
+    int i = (left - 1);
+    int j = left;
+    for (j = left; j <= right - 1; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[right]);
+    return (i + 1);
+}
+
+void quickSort(int *array, int left, int right) {
+    if (left < right) {
+        int pivot;
+        pivot = partition(array, left, right);
+        quickSort(array, left, pivot - 1);
+        quickSort(array, pivot + 1, right);
     }
 }
 
@@ -49,8 +72,10 @@ int main() {
     int n = sizeof(data) / sizeof(data[0]);
 //    bubbleSort(data, n);
 //    insertionSort(data, n);
-    selectionSort(data, n);
+//    selectionSort(data, n);
+    quickSort(data, 0, n - 1);
 
+    printf("\n\n");
     for (int i = 0; i < n; i++) {
         printf("%d, ", data[i]);
     }
