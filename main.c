3f7ecc363f634e08337548bf3b6400c1d8f1a249
jbeynar <jakub.bejnarowicz@gmail.com>
2018-06-05 09:05:50 +0200

    heap sort

--- main.c
+++ main.c
@@ -77,6 +77,32 @@ void quickSort(int *array, int left, int right) {
     }
 }
 
+void heapify(int *arr, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int *array, int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(array, n, i);
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        swap(&array[0], &array[i]);
+        heapify(array, i, 0);
+    }
+}
+
 int getRandomNumber() {
     return (rand() % 200) - 100;
 }
